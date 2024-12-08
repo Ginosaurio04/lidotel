c6ab3c0e1e7d7a38c2f6f759a437b95efd70ee48
Gino Cova <gcova.1249@unimar.edu.ve>
2024-12-07 21:52:20 -0400

    inicio

---
+++ lidotel.pas
@@ -0,0 +1,17 @@
+program lidotel;
+type
+cliente: record;
+nombre: string;
+apellido: String;
+end;
+habitacion: record;
+numero: Integer;
+tipo: string;
+renta: Boolean
+end;
+reservacion: record;
+numero: Integer;
+habitacion: habitacion;
+cliente: cliente;
+fecha: Word
+
